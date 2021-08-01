a881a8c684591b1b6c2ee3cf48ec70edec19c901
兰世豪 <1183643226@qq.com>
2021-08-02 01:08:22 +0800

    feat(router and vuex): add vue-router and vuex

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: () => {
+    return {
+      name: 'thornsh'
+    }
+  },
+  mutations: {},
+  getters: {},
+  actions: {}
+})
+
+export default store
